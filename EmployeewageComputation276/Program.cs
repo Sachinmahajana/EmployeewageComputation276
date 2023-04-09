02a80df29064cd669e5f597e67af790698547406
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-09 23:18:59 +0530

    [Sachiin] Adding Employeeattendence in program

--- EmployeewageComputation276/Program.cs
+++ EmployeewageComputation276/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage Computation Program");
+            //UC1-Employeeattendence
+            int isPresent = 1;
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2);
+
+            if(empAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
