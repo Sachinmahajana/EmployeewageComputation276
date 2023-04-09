de0042bfe778ae03a48785e5f230c8e27cb7758a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-09 23:04:19 +0530

    [Sachin] Adding Welcome message

--- EmployeewageComputation276/Program.cs
+++ EmployeewageComputation276/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employwage Computation Program");
+            Console.WriteLine("Welcome to Employeewage Computation Program");
         }
     }
 }
