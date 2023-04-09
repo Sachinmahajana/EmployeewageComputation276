d0caddafdff1d0b9f28c64f945b353008b30827a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-09 23:43:36 +0530

    [Sachiin] Adding UC2-DailyEmployeewage in a program

--- EmployeewageComputation276/Program.cs
+++ EmployeewageComputation276/Program.cs
@@ -5,19 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage Computation Program");
-            //UC1-Employeeattendence
-            int isPresent = 1;
+            //UC2-Employeedailywage
+            const int FULL_TIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0, empWage = 0;
             Random random = new Random();
             int empAttendence = random.Next(0, 2);
 
-            if(empAttendence == isPresent)
+            if(empAttendence == FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employeewage {0} wage", empWage);
             Console.ReadLine();
         }
     }
