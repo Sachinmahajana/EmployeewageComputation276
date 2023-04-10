a5781167867233e665aaf8b0d41ea7cb16eb8339
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-10 12:35:14 +0530

    [Sachin] Adding Parttime Employeewage

--- EmployeewageComputation276/Program.cs
+++ EmployeewageComputation276/Program.cs
@@ -5,17 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage Computation Program");
-            //UC2-Employeedailywage
+            //UC3-AddParttime Employeedailywage
             const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             int empHrs = 0, empWage = 0;
             Random random = new Random();
-            int empAttendence = random.Next(0, 2);
+            int empAttendence = random.Next(0, 3);
 
-            if(empAttendence == FULL_TIME)
+            if (empAttendence == FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
+            }
+            else if (empAttendence == PART_TIME)
+            {
+                Console.WriteLine("Part time Employee is Present");
+                empHrs = 4;
             }
             else
             {
