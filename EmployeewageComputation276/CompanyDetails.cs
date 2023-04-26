505a3e70603d92542329b15c036147222ae276d6
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-26 08:25:00 +0530

    [Sachin] Adding UC10-ManageEmpWageAtMultipleCompanies

---
+++ EmployeewageComputation276/CompanyDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageComputation276
+{
+    public class CompanyDetails
+    {
+        public string companyName;
+        public int empRatePerHr, numberofWorkingDays, maxWorkingHrs, totalWage;
+        public CompanyDetails(string companyName, int empRatePerHr, int numberofWorkingDays, int maxWorkingHrs)
+        {
+            this.companyName = companyName;
+            this.empRatePerHr = empRatePerHr;
+            this.numberofWorkingDays = numberofWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+        }
+        public void SetTotalWage(int totalWage)
+        {
+            this.totalWage = totalWage;
+        }
+    }
+}
---
+++ EmployeewageComputation276/EmpWageBuilder.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace EmployeewageComputation276
+{
+    public class EmpWageBuilder
+    {
+
+        // public int[] arr = new int[5];
+        public CompanyDetails[] companies;
+        public int numberofcompanies = 0;
+        public EmpWageBuilder()
+        {
+            companies = new CompanyDetails[5];
+        }
+        public void AddCompanies(string companyName, int empRatePerHr, int numberofWorkingDays, int maxWorkingHrs)
+        {
+            CompanyDetails company = new CompanyDetails(companyName, empRatePerHr, numberofWorkingDays, maxWorkingHrs);
+            companies[numberofcompanies] = company;
+            numberofcompanies++;
+        }
+        public void FetchCompanyDetailsFromArray()
+        {
+            for(int i = 0; i < companies.Length; i++)
+            {
+                if (companies[i] != null)
+                {
+                    int totalWage = CalculateEmpWage(companies[i]);
+                    companies[i].SetTotalWage(totalWage);
+                }
+            }
+        }
+        public int CalculateEmpWage(CompanyDetails companyDetails)
+        { 
+            //UC6-Calculatewageoftotal Workinghrs
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            //const int EMP_RATE_PER_HR = 20;
+            //const int NUMBER_OF_WORKING_DAYS = 20;
+            //const int MAX_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalHrs = 0;
+            Random random = new Random();
+            while (day <= companyDetails.numberofWorkingDays && totalHrs < companyDetails.maxWorkingHrs)
+            {
+                int empAttendence = random.Next(0, 3);
+                switch (empAttendence)
+                {
+                    case FULL_TIME:
+                        Console.WriteLine("Employee is Present");
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        Console.WriteLine("Employee is Parttime Present");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                empWage = companyDetails.empRatePerHr*empHrs;
+
+                //Console.WriteLine("Employeewage for day{0} and {1} hrs is:{2}", day, totalHrs, empWage);
+                day++;
+                totalWage = totalWage + empWage;
+                totalHrs = totalHrs + empHrs;
+            }
+            Console.WriteLine("Totalwage for {0} {1} days and {2} hrs is:{3}", companyDetails.companyName, (day - 1), totalHrs, totalWage);
+            Console.ReadLine();
+            return totalWage;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage Computation Program");
+            EmpWageBuilder empwageBuilder = new EmpWageBuilder();
+            empwageBuilder.AddCompanies("Deloite", 40, 23, 60);
+            empwageBuilder.AddCompanies("Microsoft", 50, 26, 70);
+            empwageBuilder.AddCompanies("Infosys", 70, 30, 59);
+            empwageBuilder.FetchCompanyDetailsFromArray();
+        }
+
+        //Create the method and call in main method
+
+    }
+}
